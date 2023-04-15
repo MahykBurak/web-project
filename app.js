443945698dcaa75fe9e7dcd6fd9195a2b4900bd5
Burak Mike <burakmahyk@gmail.com>
2023-04-15 03:21:43 +0300

    feat: craeted consumer part function

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 import { setInfo,loadAC, loadLights } from "./utils/index.js"
 const mode = localStorage.getItem('mode')
 setInfo()
+
+const object = {
+  ac:{
+    heat:22,
+    air:true,
+    cold:true,
+    hot:false
+  },
+  lights:{
+    livingRoom:false,
+    kitchen:true,
+    saloon:false,
+    bath:true
+  }
+}
+function consumer(){
+  const options = JSON.parse(localStorage.getItem('options'))
+  if(options){
+    const { ac, lights } = options
+    loadAC(ac)
+    loadLights(lights)
+  }
+}
+consumer()
