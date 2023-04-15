04cd16f1a80bcfcbb1c528d401f2a79316fe32fa
Burak Mike <burakmahyk@gmail.com>
2023-04-15 03:29:21 +0300

    prettier

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-import { setInfo,loadAC, loadLights } from "./utils/index.js"
-const mode = localStorage.getItem('mode')
-setInfo()
+import { setInfo, loadAC, loadLights } from "./utils/index.js";
+const mode = localStorage.getItem("mode");
+setInfo();
 
 const object = {
-  ac:{
-    heat:22,
-    air:true,
-    cold:true,
-    hot:false
+  ac: {
+    heat: 22,
+    air: true,
+    cold: true,
+    hot: false,
   },
-  lights:{
-    livingRoom:false,
-    kitchen:true,
-    saloon:false,
-    bath:true
+  lights: {
+    livingRoom: false,
+    kitchen: true,
+    saloon: false,
+    bath: true,
+  },
+};
+function consumer() {
+  const options = JSON.parse(localStorage.getItem("options"));
+  if (options) {
+    const { ac, lights } = options;
+    loadAC(ac);
+    loadLights(lights);
   }
 }
-function consumer(){
-  const options = JSON.parse(localStorage.getItem('options'))
-  if(options){
-    const { ac, lights } = options
-    loadAC(ac)
-    loadLights(lights)
-  }
-}
-if(mode === 'consumer'){
-  consumer()
+if (mode === "consumer") {
+  consumer();
 }
--- utils/index.js
+++ utils/index.js
@@ -1,85 +1,84 @@
-function setInfo(){
-  const date = new Date()
-  const currentTime = `${date.getHours()}.${date.getMinutes()}`
-  const todayName = date.toLocaleString('en-us', { weekday: 'long' })
-  const today = date.getDay()
-  const dayNumberContainer = document.getElementById('day-number')
-  const dayTextContainer = document.getElementById('day-text')
-  const timeContainer = document.getElementById('time')
-  dayNumberContainer.innerHTML = today
-  dayTextContainer.innerHTML = todayName
-timeContainer.innerHTML = currentTime
+function setInfo() {
+  const date = new Date();
+  const currentTime = `${date.getHours()}.${date.getMinutes()}`;
+  const todayName = date.toLocaleString("en-us", { weekday: "long" });
+  const today = date.getDay();
+  const dayNumberContainer = document.getElementById("day-number");
+  const dayTextContainer = document.getElementById("day-text");
+  const timeContainer = document.getElementById("time");
+  dayNumberContainer.innerHTML = today;
+  dayTextContainer.innerHTML = todayName;
+  timeContainer.innerHTML = currentTime;
 }
 
-function loadAC(ac){
-  const { heat, air, cold, hot } = ac
-  const heatValue = document.getElementById('heat-value')
-  const heatControlAir = document.getElementById('heat-control-air')
-  const heatControlCold = document.getElementById('heat-control-cold')
-  const heatControlHot = document.getElementById('heat-control-hot')
-  const AirActiveIcon = '<img src="assets/air.png"/>'
-  const AirInactiveIcon ='<img src="assets/air-inactive.png"/>'
-  const ColdActiveIcon = '<img src="assets/cold.png"/>'
-  const ColdInactiveIcon = '<img src="assets/cold-inactive.png"/>'
-  const HotActiveIcon = '<img src="assets/sun.png"/>'
-  const HotInactiveIcon = '<img src="assets/sun-inactive.png"/>'
-  heatValue.innerHTML = heat
-  if(air){
-    heatControlAir.classList.add('active-control')
-    heatControlAir.innerHTML = AirActiveIcon
-  }else{
-    heatControlAir.classList.remove('active-control')
-    heatControlAir.classList.add('inactive-control')
-    heatControlAir.innerHTML = AirInactiveIcon
+function loadAC(ac) {
+  const { heat, air, cold, hot } = ac;
+  const heatValue = document.getElementById("heat-value");
+  const heatControlAir = document.getElementById("heat-control-air");
+  const heatControlCold = document.getElementById("heat-control-cold");
+  const heatControlHot = document.getElementById("heat-control-hot");
+  const AirActiveIcon = '<img src="assets/air.png"/>';
+  const AirInactiveIcon = '<img src="assets/air-inactive.png"/>';
+  const ColdActiveIcon = '<img src="assets/cold.png"/>';
+  const ColdInactiveIcon = '<img src="assets/cold-inactive.png"/>';
+  const HotActiveIcon = '<img src="assets/sun.png"/>';
+  const HotInactiveIcon = '<img src="assets/sun-inactive.png"/>';
+  heatValue.innerHTML = heat;
+  if (air) {
+    heatControlAir.classList.add("active-control");
+    heatControlAir.innerHTML = AirActiveIcon;
+  } else {
+    heatControlAir.classList.remove("active-control");
+    heatControlAir.classList.add("inactive-control");
+    heatControlAir.innerHTML = AirInactiveIcon;
   }
-  if(cold){
-    heatControlCold.classList.add('active-control')
-    heatControlCold.innerHTML = ColdActiveIcon
-  }else{
-    heatControlCold.classList.remove('active-control')
-    heatControlCold.classList.add('inactive-control')
-    heatControlCold.innerHTML = ColdInactiveIcon
+  if (cold) {
+    heatControlCold.classList.add("active-control");
+    heatControlCold.innerHTML = ColdActiveIcon;
+  } else {
+    heatControlCold.classList.remove("active-control");
+    heatControlCold.classList.add("inactive-control");
+    heatControlCold.innerHTML = ColdInactiveIcon;
   }
-  if(hot){
-    heatControlHot.classList.add('active-control')
-    heatControlHot.innerHTML = HotActiveIcon
-  }else{
-    heatControlHot.classList.remove('active-control')
-    heatControlHot.classList.add('inactive-control')
-    heatControlHot.innerHTML = HotInactiveIcon
-  }
-}
-
-
-function loadLights(lights){
-  const {kitchen, livingRoom, saloon, bath}=lights
-  const lightsKitchenImg = document.getElementById('lights-kitchen-img')
-  const lightsLivingRoomImg = document.getElementById('lights-living-img')
-  const lightsSaloonImg = document.getElementById('lights-saloon-img')
-  const lightsBathImg = document.getElementById('lights-bath-img')
-  const lightsKitchenActiveIcon = '<img src="assets/lights-on.png"/>'
-  const lightsKitchenInactiveIcon = '<img src="assets/lights-off.png"/>'
-
-  if(kitchen){
-    lightsKitchenImg.innerHTML=lightsKitchenActiveIcon
-  }else{
-    lightsKitchenImg.innerHTML=lightsKitchenInactiveIcon
-  }
-  if(livingRoom){
-    lightsLivingRoomImg.innerHTML=lightsKitchenActiveIcon
-  }else{
-    lightsLivingRoomImg.innerHTML=lightsKitchenInactiveIcon
-  }
-  if(saloon){
-    lightsSaloonImg.innerHTML=lightsKitchenActiveIcon
-  }else{
-    lightsSaloonImg.innerHTML=lightsKitchenInactiveIcon
-  }
-  if(bath){
-    lightsBathImg.innerHTML=lightsKitchenActiveIcon
-  }else{
-    lightsBathImg.innerHTML=lightsKitchenInactiveIcon
+  if (hot) {
+    heatControlHot.classList.add("active-control");
+    heatControlHot.innerHTML = HotActiveIcon;
+  } else {
+    heatControlHot.classList.remove("active-control");
+    heatControlHot.classList.add("inactive-control");
+    heatControlHot.innerHTML = HotInactiveIcon;
   }
 }
 
-export { setInfo,loadAC ,loadLights}
+function loadLights(lights) {
+  const { kitchen, livingRoom, saloon, bath } = lights;
+  const lightsKitchenImg = document.getElementById("lights-kitchen-img");
+  const lightsLivingRoomImg = document.getElementById("lights-living-img");
+  const lightsSaloonImg = document.getElementById("lights-saloon-img");
+  const lightsBathImg = document.getElementById("lights-bath-img");
+  const lightsKitchenActiveIcon = '<img src="assets/lights-on.png"/>';
+  const lightsKitchenInactiveIcon = '<img src="assets/lights-off.png"/>';
+
+  if (kitchen) {
+    lightsKitchenImg.innerHTML = lightsKitchenActiveIcon;
+  } else {
+    lightsKitchenImg.innerHTML = lightsKitchenInactiveIcon;
+  }
+  if (livingRoom) {
+    lightsLivingRoomImg.innerHTML = lightsKitchenActiveIcon;
+  } else {
+    lightsLivingRoomImg.innerHTML = lightsKitchenInactiveIcon;
+  }
+  if (saloon) {
+    lightsSaloonImg.innerHTML = lightsKitchenActiveIcon;
+  } else {
+    lightsSaloonImg.innerHTML = lightsKitchenInactiveIcon;
+  }
+  if (bath) {
+    lightsBathImg.innerHTML = lightsKitchenActiveIcon;
+  } else {
+    lightsBathImg.innerHTML = lightsKitchenInactiveIcon;
+  }
+}
+
+export { setInfo, loadAC, loadLights };
