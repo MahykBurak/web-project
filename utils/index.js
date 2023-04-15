4545f45d24120151cbd2e2d4e74dbea50233e742
Burak Mike <burakmahyk@gmail.com>
2023-04-15 03:19:54 +0300

    feat: added loadLights function

--- utils/index.js
+++ utils/index.js
@@ -10,3 +10,35 @@ function setInfo(){
   dayTextContainer.innerHTML = todayName
 timeContainer.innerHTML = currentTime
 }
+function loadLights(lights){
+  const {kitchen, livingRoom, saloon, bath}=lights
+  const lightsKitchenImg = document.getElementById('lights-kitchen-img')
+  const lightsLivingRoomImg = document.getElementById('lights-living-img')
+  const lightsSaloonImg = document.getElementById('lights-saloon-img')
+  const lightsBathImg = document.getElementById('lights-bath-img')
+  const lightsKitchenActiveIcon = '<img src="assets/lights-on.png"/>'
+  const lightsKitchenInactiveIcon = '<img src="assets/lights-off.png"/>'
+
+  if(kitchen){
+    lightsKitchenImg.innerHTML=lightsKitchenActiveIcon
+  }else{
+    lightsKitchenImg.innerHTML=lightsKitchenInactiveIcon
+  }
+  if(livingRoom){
+    lightsLivingRoomImg.innerHTML=lightsKitchenActiveIcon
+  }else{
+    lightsLivingRoomImg.innerHTML=lightsKitchenInactiveIcon
+  }
+  if(saloon){
+    lightsSaloonImg.innerHTML=lightsKitchenActiveIcon
+  }else{
+    lightsSaloonImg.innerHTML=lightsKitchenInactiveIcon
+  }
+  if(bath){
+    lightsBathImg.innerHTML=lightsKitchenActiveIcon
+  }else{
+    lightsBathImg.innerHTML=lightsKitchenInactiveIcon
+  }
+}
+
+export { setInfo,loadAC ,loadLights}
