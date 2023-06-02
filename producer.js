f47bae8f3635acd182006f4b073f6e8a79be3ee7
Burak Mike <burakmahyk@gmail.com>
2023-06-03 02:25:18 +0300

    feat: lights controller added

--- producer.js
+++ producer.js
@@ -1,3 +1,4 @@
+//tempature-start
 const tempature_room = document.getElementById('tempature_room_select')
 const tempature_isOpen = document.getElementById('tempature_isOpen')
 const tempature_range = document.getElementById('tempature_range')
@@ -22,4 +23,28 @@ tempature_heatMode_isCold.addEventListener("change", (event) => {
 tempature_fan_isOpen.addEventListener("change", (event) => {
     const val = event.target.checked
     localStorage.setItem('tempature_fan_isOpen',val)
+})
+//tempature-end
+
+//light-start
+const lights_livingRoom_isOpen = document.getElementById('lights_livingRoom_isOpen')
+const lights_kitchen_isOpen = document.getElementById('lights_kitchen_isOpen')
+const lights_bedroom_isOpen = document.getElementById('lights_bedroom_isOpen')
+const lights_bath_isOpen = document.getElementById('lights_bath_isOpen')
+lights_livingRoom_isOpen.addEventListener("change", (event) => {
+    const val = event.target.checked
+    localStorage.setItem('lights_livingRoom_isOpen',val)
+})
+lights_kitchen_isOpen.addEventListener("change", (event) => {
+    const val = event.target.checked
+    localStorage.setItem('lights_kitchen_isOpen',val)
+})
+
+lights_bedroom_isOpen.addEventListener("change", (event) => {
+    const val = event.target.checked
+    localStorage.setItem('lights_bedroom_isOpen',val)
+})
+lights_bath_isOpen.addEventListener("change", (event) => {
+    const val = event.target.checked
+    localStorage.setItem('lights_bath_isOpen',val)
 })
